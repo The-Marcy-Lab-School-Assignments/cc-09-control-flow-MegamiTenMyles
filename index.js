a86552a51382bc5144e226cd0d952e0c90909edb
MegamiTenMyles <mylesmatthews2112@gmail.com>
2021-10-27 14:32:06 +0000

    questions answered 10-27-2021 10:32AM

--- index.js
+++ index.js
@@ -1,30 +1,80 @@
 //1
-function betweenFiveAndTen(){
-  
+function betweenFiveAndTen(num){
+  //This asks if Num is greater than or equal to 5 AND less than or equal to 10
+  if (num >= 5 && num <= 10) {
+   // This will tell the if statement to return true if both of these conditions are met 
+   return true
+  } else {
+      //This code says that all other numbers that fall outside of the earlier conditions, will be met with false.
+      return false
+  }
 }
 
 //2
-function betweenTenAndThirty(){
-  
+function betweenTenAndThirty(num){
+//This asks if Num is greater than or equal to 10 AND less than or equal to 30
+  if (num >= 10 && num <= 30) {
+   // This will tell the if statement to return true if both of these conditions are met 
+   return true
+  } else {
+      //This code says that all other numbers that fall outside of the earlier conditions, will be met with false.
+      return false
+  }
 }
 
 //3
-function betweenThirtyAndFifty(){
-  
+function betweenThirtyAndFifty(num){
+//This asks if Num is greater than or equal to 30 AND less than or equal to 50
+  if (num >= 30 && num <= 50) {
+   // This will tell the if statement to return true if both of these conditions are met 
+   return true
+  } else {
+      //This code says that all other numbers that fall outside of the earlier conditions, will be met with false.
+      return false
+  }
 }
 
-
 //4
-function betweenThirtyFiftySeventy(){
-  
+function betweenThirtyFiftySeventy(num){
+//This asks if Num is greater than or equal to 0 AND less than or equal to 30
+  if (num >= 0 && num <= 30) {
+   // This will tell the if statement to return true if both of these conditions are met 
+   return true
+   // This is adding a second conditional of greater than or equal to 50 and less than or equal to 70
+  } else if (num >= 50 && num <= 70){
+    return true
+      //This code says that all other numbers that fall outside of the earlier conditions, will be met with false.
+  } else {
+      return false
+  }
 }
 
 //5
-function betweenTwentyFiftySixty(){
-  
+function betweenTwentyFiftySixty(num){
+ //This asks if Num is greater than or equal to 0 AND less than or equal to 20
+  if (num >= 0 && num <= 20) {
+   // This will tell the if statement to return true if both of these conditions are met 
+   return true
+   // This is adding a second conditional of greater than or equal to 50 and less than or equal to 60
+  } else if (num >= 50 && num <= 60){
+    return true
+      //This code says that all other numbers that fall outside of the earlier conditions, will be met with false.
+  } else {
+      return false
+  }
 }
 
 //6
-function betweenTenTwentyThirty(){
-  
+function betweenTenTwentyThirty(num){
+ //This asks if Num is greater than or equal to 0 AND less than or equal to 10
+  if (num >= 0 && num <= 10) {
+   // This will tell the if statement to return true if both of these conditions are met 
+   return true
+   // This is adding a second conditional of greater than or equal to 20 and less than or equal to 30
+  } else if (num >= 20 && num <= 30){
+    return true
+      //This code says that all other numbers that fall outside of the earlier conditions, will be met with false.
+  } else {
+      return false
+  }
 }
